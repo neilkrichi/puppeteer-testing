9b022c1f93074348f7562db89ab72b6543b95f0a
neilkrichi <neilkrichi@gmail.com>
2018-04-15 02:43:43 -0700

    replicated user activity w faker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import Login from './Login.js'
+import SuccessMessage from './SuccessMessage.js'
 import './style.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      complete: false
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({complete: true})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 data-testid="h1" className="App-title">Welcome to React</h1>
+          <nav data-testid="navbar" className='navbar' role='navigation'>
+            <ul>
+              <li data-testid="navBarLi" className='nav-li'><a href='#'>Home</a></li>
+              <li data-testid="navBarLi" className='nav-li'><a href='#'>About</a></li>
+              <li data-testid="navBarLi" className='nav-li'><a href='#'>Skills</a></li>
+              <li data-testid="navBarLi" className='nav-li'><a href='#'>Works</a></li>
+            </ul>
+          </nav>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        { this.state.complete ?
+          <SuccessMessage />
+          : <Login submit={this.handleSubmit} />
+        }
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,71 @@
 const puppeteer = require('puppeteer')
+const faker = require('faker')
+
+const user = {
+  email: faker.internet.email(),
+  password: 'test',
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName()
+}
 
 const isDebugging = () => {
   const debugging_mode = {
     headless: false,
-    slowMo: 250,
+    slowMo: 50,
     devtools: true
   }
   return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
 }
 
+let browser
+let page
+
+beforeAll( async() => {
+  browser = await puppeteer.launch(isDebugging())
+  page = await browser.newPage()
+  await page.goto('http://localhost:3000/')
+  page.setViewport({ width: 500, height: 2400 })
+})
+
 describe('on page load', () => {
   test('h1 loads correctly', async () => {
-    let browser = await puppeteer.launch(isDebugging())
-    let page = await browser.newPage()
 
-    page.emulate({
-      viewport: {
-        width: 500,
-        height: 2400
-      },
-      userAgent: ''
-    })
-
-    await page.goto('http://localhost:3000/')
-
-    const html = await page.$eval('.App-title', e => e.innerHTML)
-
+    const html = await page.$eval('[data-testid="h1"]', e => e.innerHTML)
     expect(html).toBe('Welcome to React')
 
-    browser.close()
   }, 16000)
+
+  test('nav loads correctly', async () => {
+
+    const navbar = await page.$eval('[data-testid="navbar"]', el => el ? true : false)
+    const listItems = await page.$$('[data-testid="navBarLi"]')
+
+    expect(navbar).toBe(true)
+    expect(listItems.length).toBe(4)
+  })
+
+  test('login form works correctly', async() => {
+    await page.click('[data-testid="firstName"]')
+    await page.type('[data-testid="firstName"]', user.firstName)
+
+    await page.click('[data-testid="lastName"]')
+    await page.type('[data-testid="lastName"]', user.lastName)
+
+    await page.click('[data-testid="email"]')
+    await page.type('[data-testid="email"]', user.email)
+
+    await page.click('[data-testid="password"]')
+    await page.type('[data-testid="password"]', user.password)
+
+    await page.click('[data-testid="submit"]')
+
+    await page.waitForSelector('[data-testid="success"]')
+  }, 20000)
+
+})
+
+afterAll(() => {
+  if (isDebugging()) {
+    browser.close()
+  }
 })
